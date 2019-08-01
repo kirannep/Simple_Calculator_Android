c64d609700a2b056a00eafbc2e037ad77168ad6e
kirannep <kiran.nepali@hotmail.com>
2019-08-01 14:06:55 +0100

    Simple project that perform arithmetic

--- app/src/main/java/com/example/calculator/MainJavaActivity.java
+++ app/src/main/java/com/example/calculator/MainJavaActivity.java
@@ -1,6 +1,7 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,9 +12,12 @@ import org.w3c.dom.Text;
 
 public class MainJavaActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainJavaActivity";
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
         Button btnSave = findViewById(R.id.btn_save);
         final EditText etName= findViewById(R.id.et_name);
@@ -93,4 +97,40 @@ public class MainJavaActivity extends AppCompatActivity {
             }
         });
     }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Log.i(TAG, "onStart: ");
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.i(TAG, "onResume: ");
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Log.i(TAG, "onPause: ");
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Log.i(TAG, "onStop: ");
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Log.i(TAG, "onDestroy: ");
+//    }
+//
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Log.i(TAG, "onRestart: ");
+//    }
 }
